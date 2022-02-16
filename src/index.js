48c4d4f0705c79d0546a7b3b9123e6d53724d3e4
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-02-16 18:45:03 -0300

    verificação de cpf que já existe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+
 const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+
 app.use(express.json())
 
 const customers = []
@@ -12,15 +14,22 @@ const customers = []
  *  statement - []
  */
 app.post('/account', (request, response) => {
-  
   const { cpf, name } = request.body
 
-  const id = uuidv4()
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+  // vai verificar se algum cpf que esta vindo post ja existe no nosso array
+  // se ecistir vai retornar true se não vai retornar false
+  // some -> vai faze uma busca baseada na condição que eu passar para ele
+
+  if (customerAlreadyExists) {
+    // se o cpf ja existir ele vai fazer retornar um erro e vai evitar que outras coisas aconteca
+    return response.status(400).json({ error: 'Customer already Exists!' })
+  }
 
   customers.push({
     cpf,
     name,
-    uiid: id,
+    id: uuidv4(),
     statement: []
   })
 
